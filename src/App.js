f9c876d43cc7dc1fc3e21677d748add545e8b6a7
tusharkr16 <tusharkumar9871@gmail.com>
2021-12-29 11:38:50 +0530

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
+import { useState } from 'react';
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
+import AboutUs from './Components/AboutUs';
 import Navbar from './Components/Navbar';
 import Textform from './Components/Textform';
 function App() {
+  const[mode,setMode]=useState('dark')
+  const toggleMode = () => {
+    if(mode ==='light'){
+      setMode('dark')
+    }
+    else{
+      setMode('light')
+    }
+  }
   return (
 <>
-<Navbar title="TextUtils" AboutText="AboutUs" />
-<Textform/>
+<Navbar title="TextUtils" AboutText="AboutUs" mode={mode} />
+<Routes> 
+  <Route path='/about' element={<AboutUs/>}/>
+  <Route path='/' element={<Textform mode={mode} toggleMode={toggleMode}/>}/>
+</Routes>
+
 </>
   )
 }
--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -9,6 +9,15 @@ function Textform() {
     const handleOnchange = (event) => {
         setText(event.target.value)
     }
+    const handleLowclick = () => {
+        let newText=text.toLowerCase();
+        setText(newText)
+}
+const CopyText = () => {
+    var text = document.getElementById("exampleFormControlTextarea1")
+    text.select();
+    navigator.clipboard.writeText(text.value);
+}
   return (
     <div className="container mt-4">
       <div className="mb-3">
@@ -22,16 +31,22 @@ function Textform() {
           rows="8"
           value={text}
         ></textarea>
-        <button type="button" onClick={handleUpclick} className="btn mt-4 btn-primary">
+        <button type="button" onClick={handleUpclick} className="btn mt-4 mx-3 btn-dark">
         Uppercase
         </button>
-        <button type="button" className="btn mt-4 mx-3 btn-secondary">
-          Secondary
+        <button type="button" onClick={handleLowclick} className="btn mt-4 mx-3 btn-dark">
+        Lowercase
+        </button>
+        <button type="button" onClick={CopyText} className="btn mt-4 mx-3 btn-dark">
+        Copy
         </button>
       </div>
       <div className="container">
           <h3>Your Text Summary</h3>
           <p>{text.split(" ").length} words and {text.length} characters</p>
+          <p>{0.008 * text.split(" ").length} minute read </p>
+          <h4>Preview</h4>
+          <p>{text}</p>
       </div>
 
     </div>
